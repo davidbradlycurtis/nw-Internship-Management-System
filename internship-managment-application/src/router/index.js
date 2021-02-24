0722516d7f058fd6fe8642c38315b0a8f11a68c8
Lindsay Gillespie <S529464@nwmissouri.edu>
2021-02-24 15:56:28 -0600

    Added in student dashboard

--- internship-managment-application/src/router/index.js
+++ internship-managment-application/src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/studentDash',
+    name: 'StudentDashboard',
+    component: () => import(/* webpackChunkName: "studentDash" */'../views/StudentDashboard.vue')
   }
 ]
 
