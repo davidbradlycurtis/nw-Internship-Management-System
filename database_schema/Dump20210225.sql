a018dc50e6484098d7f67acb44b020a0df9c646f
Curtis <S531274@nwmissouri.edu>
2021-02-25 13:52:23 -0600

    Add review and users table to db

--- database_schema/Dump20210225.sql
+++ database_schema/Dump20210225.sql
@@ -128,6 +128,33 @@ LOCK TABLES `internships` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `review`
+--
+
+DROP TABLE IF EXISTS `review`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `review` (
+  `admin_id` int NOT NULL,
+  `application_number` int NOT NULL,
+  `date_of_review` varchar(45) DEFAULT NULL,
+  KEY `fk_review_admin1_idx` (`admin_id`),
+  KEY `fk_review_application1_idx` (`application_number`),
+  CONSTRAINT `fk_review_admin1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`),
+  CONSTRAINT `fk_review_application1` FOREIGN KEY (`application_number`) REFERENCES `application` (`application_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `review`
+--
+
+LOCK TABLES `review` WRITE;
+/*!40000 ALTER TABLE `review` DISABLE KEYS */;
+/*!40000 ALTER TABLE `review` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `student`
 --
 
@@ -140,7 +167,7 @@ CREATE TABLE `student` (
   `student_firstname` varchar(25) DEFAULT NULL,
   `student_location` varchar(20) DEFAULT NULL,
   `major` varchar(25) DEFAULT NULL,
-  `919Number` int DEFAULT NULL,
+  `sid_number` int DEFAULT NULL,
   `semester` varchar(10) DEFAULT NULL,
   `graduation` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`student_id`)
@@ -155,6 +182,31 @@ LOCK TABLES `student` WRITE;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `email` varchar(40) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `role` varchar(10) NOT NULL,
+  `user_id` varchar(45) NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -165,4 +217,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-02-23 15:54:50
+-- Dump completed on 2021-02-25 13:49:21
