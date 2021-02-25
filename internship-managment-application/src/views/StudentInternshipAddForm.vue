fef8921172a50db65c16fde67ca259039f9f65b3
Thomas Tran <thomastran707@gmail.com>
2021-02-25 13:24:41 -0600

    added in student add form

---
+++ internship-managment-application/src/views/StudentInternshipAddForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="sidenav">
+    <Sidebar/>
+    <div class="studentDash">
+      <h1>Student Add Form</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar.vue'
+
+export default {
+  name: 'sidenav',
+  components: {
+    Sidebar
+  }
+}
+</script>
+
+<style lang="scss">
+// places sidebar to the left
+ .sidenav{
+  display:grid;
+  grid-template-columns: 1fr 6fr;
+  height: 150vh;
+  width: 100vw;
+  }
+</style>
