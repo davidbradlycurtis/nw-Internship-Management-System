7c3964bd5a1c54d7746cb8abc50825d1a303c280
Curtis <S531274@nwmissouri.edu>
2021-03-03 09:36:10 -0600

    Add student-info-form stub

--- internship-managment-application/src/views/StudentInternshipApplication.vue
+++ internship-managment-application/src/views/StudentInternshipApplication.vue
@@ -1,17 +1,27 @@
 <template>
     <div class="studentDash">
-      <h1>Student Intership Application</h1>
+      <h1 class="title">Student Intership Application</h1>
+      <InternshipApplicationStudentForm/>
     </div>
 </template>
 
 <script>
 
+import InternshipApplicationStudentForm from '../components/InternshipApplicationStudentForm.vue'
 export default {
   name: 'StudentInternshipApplication.vue',
   components: {
+    InternshipApplicationStudentForm
   }
 }
 </script>
 
 <style lang="scss">
+.title {
+  text-align: center;
+  font: normal normal bold 48px/63px Futura;
+  letter-spacing: 0px;
+  color: #006747;
+  opacity: 1;
+}
 </style>
