1b194a48d36c354a8e5d540b3fbf8c37b3d58710
Sneha Ojha <42754345+osneha@users.noreply.github.com>
2021-03-03 13:08:24 -0600

    changes in footer

--- internship-managment-application/src/components/Footer.vue
+++ internship-managment-application/src/components/Footer.vue
@@ -1,14 +1,26 @@
 <template>
   <!-- Footer -->
-  <div class="footer-main-div">
-    <ul>
-       <li> &copy; Copyright 2021 Dream Team All Rights Reserved </li>
-        <a><router-link to="/student-internship-add-form">Internship Add Form</router-link></a> |
-        <a><router-link to="/student-internship-application">Internship Application</router-link></a> |
-        <a><router-link to="/student-review-status">Review Status</router-link></a> |
-        <a><router-link to="/student-internship-agreement">Internship Agreement</router-link></a>
-      </ul>
+  <mdb-footer color="black" class="font-medium pt-4 mt-4">
+      <div class="footer">
+      <mdb-container fluid>
+        &copy; Copyright 2021 Dream Team All Rights Reserved
+      </mdb-container>
     </div>
+    <mdb-container class="text-left">
+            <div class="footer-main-div">
+            <ul>
+                  <router-link to="/student-internship-add-form">Internship Add Form</router-link> |
+                    <router-link to="/student-internship-application">Internship Application</router-link> |
+                    <router-link to="/student-review-status">Review Status</router-link> |
+                    <router-link to="/student-internship-agreement">Internship Agreement</router-link>
+                 <!--<a href="#">Internship Add Form</a> |
+                 <a href="#">Internship Application</a> |
+                 <a href="#">Review Status</a> |
+                 <a href="#">Internship Agreement</a> | -->
+              </ul>
+        </div>
+    </mdb-container>
+  </mdb-footer>
   <!-- Footer -->
 </template>
 <script>
