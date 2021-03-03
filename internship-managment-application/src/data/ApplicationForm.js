904807f25f14d36e5cc8f853570bd06b686ec9ca
davidbradlycurtis <s531274@nwmissouri.edu>
2021-03-03 15:27:56 -0600

    Remove bad form and uncomment Lindsays good code

--- internship-managment-application/src/data/ApplicationForm.js
+++ internship-managment-application/src/data/ApplicationForm.js
@@ -1,67 +1,67 @@
 export default [
-{
-      group_name: 'Organization Information',
-      inputs: [
-        {
-          name: 'Organization Name',
-          type: 'text',
-          width: 'half-width'
-        },
-        {
-          name: 'Department of Org',
-          type: 'text',
-          width: 'half-width'
-        },
-        {
-          name: 'Street Line 1',
-          type: 'text',
-          width: 'half-width'
-        },
-        {
-          name: 'City',
-          type: 'text',
-          width: 'half-width'
-        },
-        {
-          name: 'ZipCode',
-          type: 'number',
-          width: 'half-width'
-        },
-        {
-          name: 'State',
-          type: 'text',
-          width: 'half-width'
-        },
-        {
-          name: 'International Country(NOT USA)',
-          type: 'text',
-          width: 'half-width'
-        },
-        {
-          name: 'Site Supervisor Name',
-          type: 'text',
-          width: 'half-width'
-        },
-        {
-          name: 'Site Supervisor Email',
-          type: 'text',
-          width: 'half-width'
-        },
-        {
-          name: 'Site phone',
-          type: 'number',
-          width: 'half-width'
-        },
-        {
-          name: 'International Phone',
-          type: 'number',
-          width: 'half-width'
-        },
-        {
-          name: 'Start Date',
-          type: 'date',
-          width: 'half-width'
-        }
-      ]
-    }
+  {
+    group_name: 'Organization Information',
+    inputs: [
+      {
+        name: 'Organization Name',
+        type: 'text',
+        width: 'half-width'
+      },
+      {
+        name: 'Department of Org',
+        type: 'text',
+        width: 'half-width'
+      },
+      {
+        name: 'Street Line 1',
+        type: 'text',
+        width: 'half-width'
+      },
+      {
+        name: 'City',
+        type: 'text',
+        width: 'half-width'
+      },
+      {
+        name: 'ZipCode',
+        type: 'number',
+        width: 'half-width'
+      },
+      {
+        name: 'State',
+        type: 'text',
+        width: 'half-width'
+      },
+      {
+        name: 'International Country(NOT USA)',
+        type: 'text',
+        width: 'half-width'
+      },
+      {
+        name: 'Site Supervisor Name',
+        type: 'text',
+        width: 'half-width'
+      },
+      {
+        name: 'Site Supervisor Email',
+        type: 'text',
+        width: 'half-width'
+      },
+      {
+        name: 'Site phone',
+        type: 'number',
+        width: 'half-width'
+      },
+      {
+        name: 'International Phone',
+        type: 'number',
+        width: 'half-width'
+      },
+      {
+        name: 'Start Date',
+        type: 'date',
+        width: 'half-width'
+      }
+    ]
+  }
 ]
--- internship-managment-application/src/views/StudentInternshipApplication.vue
+++ internship-managment-application/src/views/StudentInternshipApplication.vue
@@ -1,28 +1,21 @@
 <template>
     <div class="studentDash">
       <h1 class="title">Student Internship Application</h1>
-      <InternshipApplicationStudentForm/>
-      // this is commented out because when we merge with the add form this file will be in there
-      // <FormGroup  v-for="group in ApplicationForm" :key="group.group_name" v-bind:groupname="group.group_name" v-bind:input="group.inputs" v-bind:note="group.note"/>
+      <FormGroup  v-for="group in ApplicationForm" :key="group.group_name" v-bind:groupname="group.group_name" v-bind:input="group.inputs" v-bind:note="group.note"/>
     </div>
 </template>
 
 <script>
-// components
-// this is commented out because when we merge with the add form this file will be in there
-// import FormGroup from '../components/FormGroup.vue'
-import InternshipApplicationStudentForm from '../components/InternshipApplicationStudentForm.vue'
-// data
-// Cant pull data when there is no components
-// import ApplicationForm from '../data/ApplicationForm.js'
+
+import FormGroup from '../components/FormGroup.vue'
+import ApplicationForm from '../data/ApplicationForm.js'
 
 export default {
   name: 'StudentInternshipApplication.vue',
   components: {
-    InternshipApplicationStudentForm
-    // FormGroup
-  }
-  // data () { return { ApplicationForm: ApplicationForm}}
+    FormGroup
+  },
+  data () { return { ApplicationForm: ApplicationForm } }
 }
 </script>
 
