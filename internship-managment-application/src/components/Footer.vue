7c77ab487c666e574971ae3d9fde228f152e267c
Sneha Ojha <42754345+osneha@users.noreply.github.com>
2021-03-03 00:54:29 -0600

    footer-is-ready

--- internship-managment-application/src/components/Footer.vue
+++ internship-managment-application/src/components/Footer.vue
@@ -2,7 +2,7 @@
   <!-- Footer -->
   <div class="footer-main-div">
     <ul>
-       &copy; Copyright 2021 Dream Team All Rights Reserved
+       <li> &copy; Copyright 2021 Dream Team All Rights Reserved </li>
         <a><router-link to="/student-internship-add-form">Internship Add Form</router-link></a> |
         <a><router-link to="/student-internship-application">Internship Application</router-link></a> |
         <a><router-link to="/student-review-status">Review Status</router-link></a> |
