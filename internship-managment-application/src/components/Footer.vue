96fc8a2cb6c895ca836c3c185c158afcd63047e9
Sneha Ojha <42754345+osneha@users.noreply.github.com>
2021-03-03 00:44:25 -0600

    changes in footer

--- internship-managment-application/src/components/Footer.vue
+++ internship-managment-application/src/components/Footer.vue
@@ -1,24 +1,15 @@
 <template>
   <!-- Footer -->
-  <mdb-footer color="blue" class="font-small pt-4 mt-4">
-      <div class="footer">
-      <mdb-container fluid>
-        &copy; Copyright 2021 Dream Team All Rights Reserved: <a href="https://www.nwmissouri.edu/"> nwmissouri </a>
-      </mdb-container>
+  <div class="footer-main-div">
+    <ul>
+       &copy; Copyright 2021 Dream Team All Rights Reserved
+        <a><router-link to="/student-internship-add-form">Internship Add Form</router-link></a> |
+        <a><router-link to="/student-internship-application">Internship Application</router-link></a> |
+        <a><router-link to="/student-review-status">Review Status</router-link></a> |
+        <a><router-link to="/student-internship-agreement">Internship Agreement</router-link></a>
+      </ul>
     </div>
-    <mdb-container class="text-left">
-        <h5 class="title">Navigation</h5>
-            <div class="footer-main-div">
-            <ul>
-                 <li><router-link to="/student-internship-add-form">Internship Add Form</router-link></li>
-                    <li><router-link to="/student-internship-application">Internship Application</router-link></li>
-                    <li><router-link to="/student-review-status">Review Status</router-link></li>
-                    <li><router-link to="/student-internship-agreement">Internship Agreement</router-link></li>
-              </ul>
-        </div>
-    </mdb-container>
-  </mdb-footer>
-<!-- Footer -->
+  <!-- Footer -->
 </template>
 <script>
 export default {
