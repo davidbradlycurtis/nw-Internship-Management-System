6fa53a99fb6642255cdc4d26fe8608ef57063920
Lindsay Gillespie <S529464@nwmissouri.edu>
2021-03-03 13:58:50 -0600

    I changed to match add form
    To make less components and sscs easier I followed the finshed add form as a model. I left all the files FormGroup.vue and all the scss in the add form to make a merger easier. I also commented out my code to allow for testing to continue

--- internship-managment-application/src/views/StudentInternshipApplication.vue
+++ internship-managment-application/src/views/StudentInternshipApplication.vue
@@ -1,22 +1,28 @@
 <template>
     <div class="studentDash">
-      <h1 class="title">Student Intership Application</h1>
+      <h1 class="title">Student Internship Application</h1>
       <InternshipApplicationStudentForm/>
-      <InternshipApplicationEmployeeForm/>
+      // this is commented out because when we merge with the add form this file will be in there
+      // <FormGroup  v-for="group in ApplicationForm" :key="group.group_name" v-bind:groupname="group.group_name" v-bind:input="group.inputs" v-bind:note="group.note"/>
     </div>
 </template>
 
 <script>
-
+// components
+// this is commented out because when we merge with the add form this file will be in there
+// import FormGroup from '../components/FormGroup.vue'
 import InternshipApplicationStudentForm from '../components/InternshipApplicationStudentForm.vue'
-import InternshipApplicationEmployeeForm from '../components/InternshipApplicationEmployeeForm.vue'
+// data
+// Cant pull data when there is no components
+// import ApplicationForm from '../data/ApplicationForm.js'
 
 export default {
   name: 'StudentInternshipApplication.vue',
   components: {
-    InternshipApplicationStudentForm,
-    InternshipApplicationEmployeeForm
+    InternshipApplicationStudentForm
+    // FormGroup
   }
+  // data () { return { ApplicationForm: ApplicationForm}}
 }
 </script>
 
