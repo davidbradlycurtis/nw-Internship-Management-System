7954791b95fddea8d3e7dddba09f895b23a46908
Caroline Finnerty <caroline@finnerty.org>
2021-03-02 19:08:14 -0600

    Form group component created.

---
+++ internship-managment-application/src/components/FormGroup.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="formgroup">
+        <h3 class="formgrouptitle">{{groupname}}</h3>
+        <!-- once we get the backend setup, replace the hard code with a for-loop -->
+        <div v-for="label in input" :key="label.name" :class="label.width"><label :for="label.name">{{label.name}}: <input :type="label.type"></label></div>
+        <p v-if="note" class="note">{{note}}</p>
+    </div>
+</template>
+<script>
+export default {
+  props: ['groupname', 'input', 'note']
+}
+</script>
--- internship-managment-application/src/views/StudentInternshipAddForm.vue
+++ internship-managment-application/src/views/StudentInternshipAddForm.vue
@@ -1,13 +1,24 @@
 <template>
     <div class="studentDash">
       <h1>Student Add Form</h1>
+      <FormGroup  v-for="group in CourseAddForm" :key="group.group_name" v-bind:groupname="group.group_name" v-bind:input="group.inputs" v-bind:note="group.note"/>
     </div>
 </template>
 <script>
+// Components
+import FormGroup from '@/components/FormGroup.vue'
+// Data
+import CourseAddForm from '@/data/CourseAddForm.js'
 
 export default {
   name: 'StudentInternshipAddForm',
   components: {
+    FormGroup
+  },
+  data () {
+    return {
+      CourseAddForm: CourseAddForm
+    }
   }
 }
 </script>
