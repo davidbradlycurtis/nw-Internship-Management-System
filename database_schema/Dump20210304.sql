c5d2ea68ed476b01acb94cd3df38acd166ae53af
Curtis <S531274@nwmissouri.edu>
2021-03-04 13:59:22 -0600

    Add new database schema

---
+++ database_schema/Dump20210304.sql
@@ -0,0 +1,305 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mydb
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `add_forms`
+--
+
+DROP TABLE IF EXISTS `add_forms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `add_forms` (
+  `form_id` int NOT NULL AUTO_INCREMENT,
+  `student_lastname` varchar(25) DEFAULT NULL,
+  `student_firstname` varchar(25) DEFAULT NULL,
+  `uid` varchar(10) DEFAULT NULL,
+  `student_email` varchar(45) DEFAULT NULL,
+  `faculty_lastname` varchar(25) DEFAULT NULL,
+  `faculty_firstname` varchar(25) DEFAULT NULL,
+  `faculty_email` varchar(25) DEFAULT NULL,
+  `signature` varchar(25) DEFAULT NULL,
+  `date` varchar(15) DEFAULT NULL,
+  `internship_application_number` int NOT NULL,
+  PRIMARY KEY (`form_id`),
+  KEY `fk_add_internship1_idx` (`internship_application_number`),
+  CONSTRAINT `fk_add_internship1` FOREIGN KEY (`internship_application_number`) REFERENCES `application` (`application_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `add_forms`
+--
+
+LOCK TABLES `add_forms` WRITE;
+/*!40000 ALTER TABLE `add_forms` DISABLE KEYS */;
+/*!40000 ALTER TABLE `add_forms` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+  `admin_id` int NOT NULL AUTO_INCREMENT,
+  `admin_lastname` varchar(25) NOT NULL,
+  `admin_firstname` varchar(25) NOT NULL,
+  PRIMARY KEY (`admin_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `agreements`
+--
+
+DROP TABLE IF EXISTS `agreements`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `agreements` (
+  `agreement_id` int NOT NULL AUTO_INCREMENT,
+  `student_name` varchar(45) DEFAULT NULL,
+  `student_address` varchar(45) DEFAULT NULL,
+  `sid` varchar(10) DEFAULT NULL,
+  `organization_name` varchar(25) DEFAULT NULL,
+  `organization_address` varchar(45) DEFAULT NULL,
+  `supervisor_name` varchar(45) DEFAULT NULL,
+  `supervisor_email` varchar(45) DEFAULT NULL,
+  `arrangements` varchar(250) DEFAULT NULL,
+  `organization_phone` varchar(15) DEFAULT NULL,
+  `student_other` varchar(250) DEFAULT NULL,
+  `sponser_other` varchar(250) DEFAULT NULL,
+  `supervisor_other` varchar(250) DEFAULT NULL,
+  `student_signature` varchar(45) DEFAULT NULL,
+  `sponser_signature` varchar(45) DEFAULT NULL,
+  `internship_application_number` int NOT NULL,
+  PRIMARY KEY (`agreement_id`),
+  KEY `fk_agreement_internship1_idx` (`internship_application_number`),
+  CONSTRAINT `fk_agreement_internship1` FOREIGN KEY (`internship_application_number`) REFERENCES `application` (`application_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `agreements`
+--
+
+LOCK TABLES `agreements` WRITE;
+/*!40000 ALTER TABLE `agreements` DISABLE KEYS */;
+/*!40000 ALTER TABLE `agreements` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `application`
+--
+
+DROP TABLE IF EXISTS `application`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `application` (
+  `application_number` int NOT NULL AUTO_INCREMENT,
+  `application_status` varchar(10) DEFAULT NULL,
+  `application_date` datetime DEFAULT NULL,
+  `date_of_approval` datetime DEFAULT NULL,
+  `approved_by` varchar(25) DEFAULT NULL,
+  `submitted` varchar(5) DEFAULT NULL,
+  `student_id` int NOT NULL,
+  PRIMARY KEY (`application_number`),
+  KEY `approved_by_fk_idx` (`approved_by`),
+  KEY `approved_by_idx` (`approved_by`),
+  KEY `fk_internship_student1_idx` (`student_id`),
+  CONSTRAINT `approved_by` FOREIGN KEY (`approved_by`) REFERENCES `faculty` (`faculty_lastname`),
+  CONSTRAINT `fk_internship_student1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `application`
+--
+
+LOCK TABLES `application` WRITE;
+/*!40000 ALTER TABLE `application` DISABLE KEYS */;
+/*!40000 ALTER TABLE `application` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `faculty`
+--
+
+DROP TABLE IF EXISTS `faculty`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `faculty` (
+  `faculty_id` int NOT NULL AUTO_INCREMENT,
+  `faculty_firstname` varchar(25) NOT NULL,
+  `faculty_lastname` varchar(25) NOT NULL,
+  `department` varchar(10) NOT NULL,
+  PRIMARY KEY (`faculty_id`,`faculty_lastname`),
+  KEY `faculty_lastname` (`faculty_lastname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `faculty`
+--
+
+LOCK TABLES `faculty` WRITE;
+/*!40000 ALTER TABLE `faculty` DISABLE KEYS */;
+/*!40000 ALTER TABLE `faculty` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `internships`
+--
+
+DROP TABLE IF EXISTS `internships`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `internships` (
+  `employer_name` varchar(25) DEFAULT NULL,
+  `duration` varchar(10) DEFAULT NULL,
+  `start_date` datetime DEFAULT NULL,
+  `end_date` datetime DEFAULT NULL,
+  `street_line_1` varchar(25) DEFAULT NULL,
+  `street_line_2` varchar(25) DEFAULT NULL,
+  `city` varchar(25) DEFAULT NULL,
+  `zip_code` varchar(10) DEFAULT NULL,
+  `department` varchar(45) DEFAULT NULL,
+  `state` varchar(25) DEFAULT NULL,
+  `supervisor_name` varchar(45) DEFAULT NULL,
+  `supervisor_email` varchar(45) DEFAULT NULL,
+  `site_phone` varchar(15) DEFAULT NULL,
+  `international_phone` varchar(25) DEFAULT NULL,
+  `notes` varchar(250) DEFAULT NULL,
+  `internship_id` int NOT NULL AUTO_INCREMENT,
+  `offer_letter` longblob,
+  `internship_application_number` int NOT NULL,
+  PRIMARY KEY (`internship_id`),
+  KEY `fk_internships_internship1_idx` (`internship_application_number`),
+  CONSTRAINT `fk_internships_internship1` FOREIGN KEY (`internship_application_number`) REFERENCES `application` (`application_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `internships`
+--
+
+LOCK TABLES `internships` WRITE;
+/*!40000 ALTER TABLE `internships` DISABLE KEYS */;
+/*!40000 ALTER TABLE `internships` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `review`
+--
+
+DROP TABLE IF EXISTS `review`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `review` (
+  `admin_id` int NOT NULL,
+  `application_number` int NOT NULL,
+  `date_of_review` varchar(45) DEFAULT NULL,
+  KEY `fk_review_admin1_idx` (`admin_id`),
+  KEY `fk_review_application1_idx` (`application_number`),
+  CONSTRAINT `fk_review_admin1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`),
+  CONSTRAINT `fk_review_application1` FOREIGN KEY (`application_number`) REFERENCES `application` (`application_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `review`
+--
+
+LOCK TABLES `review` WRITE;
+/*!40000 ALTER TABLE `review` DISABLE KEYS */;
+/*!40000 ALTER TABLE `review` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `student_id` int NOT NULL AUTO_INCREMENT,
+  `student_lastname` varchar(25) DEFAULT NULL,
+  `student_firstname` varchar(25) DEFAULT NULL,
+  `student_location` varchar(20) DEFAULT NULL,
+  `major` varchar(25) DEFAULT NULL,
+  `sid_number` varchar(10) DEFAULT NULL,
+  `semester` varchar(10) DEFAULT NULL,
+  `graduation` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `email` varchar(40) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `role` varchar(10) NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-04 13:56:18
