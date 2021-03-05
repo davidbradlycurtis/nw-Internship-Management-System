81b0a0d823ae2798f9a07c18223c6e381f3f10dd
Thomas Tran <thomastran707@gmail.com>
2021-03-05 14:23:04 -0600

    Added DashGroup
    Added DashGroup to components
    Still a work in progress

---
+++ internship-managment-application/src/components/DashGroup.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="dashgroup">
+      <h3 class="internshiptitle">{{title}}</h3>
+      <div class="flex-blocks">
+        <div class="col" v-for="block in blocks" :key="block.title">
+          <h4 class="formtitle">{{block.title}}</h4>
+          <h5 class="status">Status</h5>
+          <p class="formstatus">{{block.status}}</p>
+        </div>
+        <div class="filling-empty-space">
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  // Place holders
+  data: () => ({
+    title: 'Insert season, year - Company',
+    blocks: [
+      { title: 'Internship course enrollment', status: 'approved' },
+      { title: 'Internship application', status: 'prending review' },
+      { title: 'Internship agreement', status: 'under review' }
+    ]
+  })
+}
+</script>
+
+<style lang="scss">
+</style>
