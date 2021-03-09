258c4ec35830f77cdfd8ab60fd21e06d4ded6c12
davidbradlycurtis <s531274@nwmissouri.edu>
2021-03-08 23:25:18 -0600

    Add student information module and save/submit button

--- internship-managment-application/src/views/StudentInternshipAgreement.vue
+++ internship-managment-application/src/views/StudentInternshipAgreement.vue
@@ -1,12 +1,37 @@
 <template>
-    <div class="">
+    <div class="studentinternshipagreement form">
+      <div class="form" v-if="!submitted">
+        <FormGroup  v-for="group in AgreementForm" :key="group.group_name" v-bind:groupname="group.group_name" v-bind:input="group.inputs" v-bind:note="group.note"/>
+        <div class="buttongroup">
+          <Button v-bind:buttontext="'Save Progress'"/>
+          <Button v-bind:buttontext="'Submit Form'" @click.native="submitted = !submitted"/>
+        </div>
+      </div>
+      <FormSubmittedPop v-if="submitted"/>
     </div>
 </template>
 
 <script>
+// Components
+import FormGroup from '@/components/FormGroup.vue'
+import Button from '@/components/Button.vue'
+import FormSubmittedPop from '@/components/FormSubmittedPop.vue'
+// Data
+import AgreementForm from '@/data/AgreementForm.js'
+
 export default {
   name: 'StudentInternshipAgreement',
   components: {
+    FormGroup,
+    Button,
+    FormSubmittedPop
+  },
+  data () {
+    return {
+      AgreementForm: AgreementForm,
+      ButtonList: ['Save Progress', 'Submit Form'],
+      submitted: false
+    }
   }
 }
 </script>
