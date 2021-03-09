62d17965c14d0f0a947c3ae019f520ea5cb7efae
davidbradlycurtis <s531274@nwmissouri.edu>
2021-03-09 09:29:39 -0600

    Add student agreement panel

--- internship-managment-application/src/data/AgreementForm.js
+++ internship-managment-application/src/data/AgreementForm.js
@@ -23,5 +23,40 @@ export default [
         width: 'half-width'
       }
     ]
+  },
+  {
+    group_name: 'Cooperating Buisness Information',
+    inputs: [
+      {
+        name: 'Organization Name',
+        type: 'text',
+        width: 'full-width'
+      },
+      {
+        name: 'Organization Address',
+        type: 'text',
+        width: 'full-width'
+      },
+      {
+        name: 'Sponsor/Supervisor',
+        type: 'text',
+        width: 'half-width'
+      },
+      {
+        name: 'Email',
+        type: 'email',
+        width: 'half-width'
+      },
+      {
+        name: 'Telephone',
+        type: 'text',
+        width: 'half-width'
+      },
+      {
+        name: 'Other Arrangements',
+        type: 'text',
+        width: 'full-width other'
+      }
+    ]
   }
 ]
--- internship-managment-application/src/views/StudentInternshipAgreement.vue
+++ internship-managment-application/src/views/StudentInternshipAgreement.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="studentinternshipagreement form">
       <div class="form" v-if="!submitted">
-        <FormGroup  v-for="group in AgreementForm" :key="group.group_name" v-bind:groupname="group.group_name" v-bind:input="group.inputs" v-bind:note="group.note"/>
+        <FormGroup v-for="group in AgreementForm" :key="group.group_name" v-bind:groupname="group.group_name" v-bind:input="group.inputs" v-bind:note="group.note"/>
+        <CustomFormGroupAgreementPanel/>
         <div class="buttongroup">
           <Button v-bind:buttontext="'Save Progress'"/>
           <Button v-bind:buttontext="'Submit Form'" @click.native="submitted = !submitted"/>
@@ -16,19 +17,23 @@
 import FormGroup from '@/components/FormGroup.vue'
 import Button from '@/components/Button.vue'
 import FormSubmittedPop from '@/components/FormSubmittedPop.vue'
+import CustomFormGroupAgreementPanel from '@/components/CustomFormGroupAgreementPanel.vue'
 // Data
 import AgreementForm from '@/data/AgreementForm.js'
+// import AgreementPanel from '@/data/AgreementPanel.js'
 
 export default {
   name: 'StudentInternshipAgreement',
   components: {
     FormGroup,
     Button,
-    FormSubmittedPop
+    FormSubmittedPop,
+    CustomFormGroupAgreementPanel
   },
   data () {
     return {
       AgreementForm: AgreementForm,
+      // AgreementPanel: AgreementPanel,
       ButtonList: ['Save Progress', 'Submit Form'],
       submitted: false
     }
