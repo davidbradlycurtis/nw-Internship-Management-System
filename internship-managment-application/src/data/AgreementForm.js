09f3773669fbf43d42c747e6ee5c9eba1ca7bcb3
davidbradlycurtis <s531274@nwmissouri.edu>
2021-03-08 23:25:18 -0600

    Add student information module and save/submit button

---
+++ internship-managment-application/src/data/AgreementForm.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    group_name: 'Student Information',
+    inputs: [
+      {
+        name: 'Student Name',
+        type: 'text',
+        width: 'full-width'
+      },
+      {
+        name: 'Address',
+        type: 'text',
+        width: 'full-width'
+      },
+      {
+        name: 'Telephone',
+        type: 'text',
+        width: 'half-width'
+      },
+      {
+        name: 'S-Number',
+        type: 'text',
+        width: 'half-width'
+      }
+    ]
+  }
+]
