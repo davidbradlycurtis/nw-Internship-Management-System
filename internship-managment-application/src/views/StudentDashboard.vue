adef497ed514928572b27180d2132f9e9260913f
Caroline Finnerty <caroline@finnerty.org>
2021-03-08 20:33:32 -0600

    There was a stray extra sidebar component

--- internship-managment-application/src/views/StudentDashboard.vue
+++ internship-managment-application/src/views/StudentDashboard.vue
@@ -1,19 +1,14 @@
 <template>
-  <div class="sidenav">
-    <Sidebar/>
     <div class="studentDash">
       <h1>Student Dashboard</h1>
     </div>
-  </div>
 </template>
 
 <script>
-import Sidebar from '@/components/Sidebar.vue'
 
 export default {
   name: 'StudentDashboard',
   components: {
-    Sidebar
   }
 }
 </script>
