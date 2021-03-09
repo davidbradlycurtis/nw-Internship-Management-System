cc44aad969b6d8b8f8e7816fde6c171597694d92
davidbradlycurtis <s531274@nwmissouri.edu>
2021-03-09 09:43:21 -0600

    Add sponsor/supervisor agreement panels

---
+++ internship-managment-application/src/components/CustomFormGroupSupervisorAgreementPanel.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="customformgroup formgroup">
+        <h3 class="formgrouptitle">The University Internship Supervisor Agrees To:</h3>
+        <div>
+          <ul>
+            <li>Assist the student in finding an internship sponsor.</li>
+            <li>Assist both the student and cooperating employer in establishing the internship and provide and explain all information concerning the program and the responsibilities of each party involved.</li>
+            <li>Maintain contact on a regular basis with the student and sponsor.</li>
+            <li>Work with the sponsor and the student to insure that the student benefits from the internship experience and the sponsor’s interests are considered.</li>
+          </ul>
+          <div class="full-width other">
+            <label>Other:</label><br>
+            <input type="text">
+          </div>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+}
+</script>
