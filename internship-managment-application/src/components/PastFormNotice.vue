66622110e5e7e4cf901acf8f7caa452593ea91b5
Lindsay Gillespie <S529464@nwmissouri.edu>
2021-03-30 17:50:17 -0500

    Added info box to past form page

---
+++ internship-managment-application/src/components/PastFormNotice.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+    <h>Past Completed Forms </h>
+    <p>You can review the information below. If you resubmit information on this form, your submission changes will be sent to your faculty supervisor for approval.
+    To start another form for additional internships, click the button labeled 'Start New Form'</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
--- internship-managment-application/src/views/StudentAgreementFormPast.vue
+++ internship-managment-application/src/views/StudentAgreementFormPast.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="pastform">
+    <PastFormNotice/>
     </div>
 </template>
 
 <script>
 
+import PastFormNotice from '@/components/PastFormNotice.vue'
 export default {
   name: 'Internship Agreement - Past Forms',
   components: {
+    PastFormNotice
   }
 }
 </script>
