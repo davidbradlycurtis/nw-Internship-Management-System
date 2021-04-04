5632f8e7e8a6709611d3d4bc2540df718bd5d529
davidbradlycurtis <s531274@nwmissouri.edu>
2021-04-04 18:39:20 -0500

    Add 404 page stub

---
+++ internship-managment-application/src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="notfound">
+      <img src="@/assets/images/N60-2Stack-W.png" alt="NW 2Stack White">
+      <h2>Sorry, the page you are trying to reach does not exist.</h2>
+      <h2>Try going back to home and repeating your action. Or, contact IT services for support.</h2>
+      <div>
+          <p><router-link to="/">Go Home</router-link></p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
+
+<style lang="scss">
+</style>
