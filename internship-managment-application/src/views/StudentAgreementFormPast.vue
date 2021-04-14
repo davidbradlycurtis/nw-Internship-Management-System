cc784b92dbbe2d67581f40ba629f4738c1fcedf0
Lindsay Gillespie <S529464@nwmissouri.edu>
2021-04-14 08:34:44 -0500

    Made changes to get database post to work
    nodemon is still empty

--- internship-managment-application/src/views/StudentAgreementFormPast.vue
+++ internship-managment-application/src/views/StudentAgreementFormPast.vue
@@ -1,14 +1,14 @@
 <template>
-<PastFormNotice/>
   <div class="pastforms form">
+  <PastFormNotice/>
     <div v-if="formdata.length >= 1">
       <PastFormInfo :data="object" :schema="schema" v-for="object in formdata" :key="object.form_id"/>
+      <div class="buttongroup">
+        <button class="formbutton"  @click="pullForm()"> Edit </button>
+      </div>
     </div>
     <div v-else>
       <UhOhNotice noticetext="It looks like you have not completed any INTERNSHIP COURSE ADD FORM's. To get started, click the link below."/>
-    </div>
-    <div class="buttongroup">
-      <button class="formbutton"  @click="pullForm()"> Edit </button>
     </div>
     <router-link to="/student-internship-agreement-form/new-form" class="inpagelink">Start New Form</router-link>
   </div>
@@ -19,15 +19,33 @@
 import AgreementFormSchema from '@/forms/AgreementFormSchema.js'
 // components
 import PastFormNotice from '@/components/PastFormNotice.vue'
+import PastFormInfo from '@/components/PastFormInfo.vue'
 import UhOhNotice from '@/components/UhOhNotice.vue'
 
 // Services
 import StudentService from '@/services/StudentService.js'
 
 export default {
-  name: 'Internship Agreement - Past Forms',
+  name: 'InternshipAgreementPastForms',
   components: {
-    PastFormNotice
+    PastFormNotice,
+    PastFormInfo,
+    UhOhNotice
+  },
+  created () {
+    this.getInternship()
+  },
+  methods: {
+    async getInternship () {
+      await StudentService.GetAgreementForm(this.model).then((result) => {
+        this.formdata = result.data[0]
+      })
+    },
+    pullForm () {
+      // rerouting to new form
+      this.$router.push('/student-internship-agreement/new-form')
+      console.log('moving')
+    }
   },
   data () {
     return {
@@ -37,21 +55,6 @@ export default {
       },
       schema: AgreementFormSchema,
       formdata: []
-    }
-  },
-  methods: {
-    async getInternship () {
-      await StudentService.GetAddForms(this.model).then((result) => {
-        this.formdata = result.data[0]
-      })
-    },
-    pullForm () {
-      // rerouting to new form
-      this.$router.push('/student-internship-agreement/new-form')
-      console.log('moving')
-    },
-    created () {
-      this.pullInternship()
     }
   }
 }
